eb55a1c788b47c905b8b489d60ef4f6ca662928c
andrija-s <51809572+andrija-s@users.noreply.github.com>
2021-11-14 21:55:19 -0800

    fixed code style a little bit

--- src/Board.java
+++ src/Board.java
@@ -12,60 +12,59 @@ public class Board {
 
   public Board() {
   }
-  
+
   // This is the combined move of player and computer,
   // yes I know it's not a great idea to couple it this way.
   public boolean makeMove(int row, int col) {
 
     row--;
     col--;
-    if (this.finished || row < 0 || row >= DIMENSION || col < 0
+    if (this.finished || row < 0
+        || row >= DIMENSION || col < 0 
         || col >= DIMENSION || this.board[row][col] != 0)
       return false;
-    else {
-      this.filled--;
-      boolean temp = this.Evaluate(row, col, 1);
-      this.board[row][col] = 1;
-      if (temp) {
-        this.winner = 1;
-        this.finished = true;
-        return true;
-      } else if (this.filled == 0) {
-        this.finished = true;
-        this.winner = 0;
-        return true;
-      }
-
-      TreeMap<Integer, int[]> order = new TreeMap<>();
-      for (int i = 0; i < DIMENSION; i++) {
-        for (int j = 0; j < DIMENSION; j++) {
-          if (this.board[i][j] == 0) {
-            int[] arr = { i, j };
-            order.put(makeMoveRec(i, j, -1, this.filled), arr);
-          }
-        }
-      }
-      // testing the result, sadly you will never see a negative key because algo
-      // can't beat itself :'(
-      /*
-       * for (Map.Entry<Integer, int[]> entry : order.entrySet()) {
-       * System.out.println(Arrays.toString(entry.getValue()) + " key: " +
-       * entry.getKey()); }
-       */
-      this.filled--;
-      boolean tempC = this.Evaluate(order.get(order.firstKey())[0], order.get(order.firstKey())[1], -1);
-      this.board[order.get(order.firstKey())[0]][order.get(order.firstKey())[1]] = -1;
-      if (tempC) {
-        this.winner = -1;
-        this.finished = true;
-        return true;
-      } else if (this.filled == 0) {
-        this.finished = true;
-        this.winner = 0;
-        return true;
-      }
+    this.filled--;
+    boolean temp = this.Evaluate(row, col, 1);
+    this.board[row][col] = 1;
+    if (temp) {
+      this.winner = 1;
+      this.finished = true;
+      return true;
+    } else if (this.filled == 0) {
+      this.finished = true;
+      this.winner = 0;
       return true;
     }
+
+    TreeMap<Integer, int[]> order = new TreeMap<>();
+    for (int i = 0; i < DIMENSION; i++) {
+      for (int j = 0; j < DIMENSION; j++) {
+        if (this.board[i][j] == 0) {
+          int[] arr = { i, j };
+          order.put(makeMoveRec(i, j, -1, this.filled), arr);
+        }
+      }
+    }
+    // testing the result, sadly you will never see a negative key because algo
+    // can't beat itself :'(
+    /*
+     * for (Map.Entry<Integer, int[]> entry : order.entrySet()) {
+     * System.out.println(Arrays.toString(entry.getValue()) + " key: " +
+     * entry.getKey()); }
+     */
+    this.filled--;
+    boolean tempC = this.Evaluate(order.get(order.firstKey())[0], order.get(order.firstKey())[1], -1);
+    this.board[order.get(order.firstKey())[0]][order.get(order.firstKey())[1]] = -1;
+    if (tempC) {
+      this.winner = -1;
+      this.finished = true;
+      return true;
+    } else if (this.filled == 0) {
+      this.finished = true;
+      this.winner = 0;
+      return true;
+    }
+    return true;
   }
 
   // Recursive search for best next move with this player
@@ -86,7 +85,7 @@ public class Board {
     this.board[row][col] = 0;
     // we want to give the weight of the strongest possible counter-attack by enemy
     // against current player, should current player not win with current move
-    if (player > 0) 
+    if (player > 0)
       return order.first();
     else
       return order.last();
@@ -100,7 +99,7 @@ public class Board {
   }
 
   /**
-   * Evaluates if move to row, col with this player is a win.
+   * Evaluates if move to row, col with this player is a win. Half of normal eval.
    * Got the idea to do it this way from StackOverflow user, Osama Al-Maadeed:
    * https://stackoverflow.com/questions/1056316/algorithm-for-determining-tic-tac-toe-game-over
    */
@@ -108,10 +107,14 @@ public class Board {
     this.board[row][col] = player;
     int c = 0, r = 0, d = 0, rd = 0;
     for (int i = 0; i < DIMENSION; i++) {
-      if (this.board[row][i] == player) c++;
-      if (this.board[i][col] == player) r++;
-      if (this.board[i][i]   == player) d++;
-      if (this.board[i][DIMENSION - i - 1] == player) rd++;
+      if (this.board[row][i] == player)
+        c++;
+      if (this.board[i][col] == player)
+        r++;
+      if (this.board[i][i] == player)
+        d++;
+      if (this.board[i][DIMENSION - i - 1] == player)
+        rd++;
     }
     this.board[row][col] = 0;
     return c == DIMENSION || r == DIMENSION || d == DIMENSION || rd == DIMENSION;
@@ -130,10 +133,10 @@ public class Board {
       result.append("\n");
     }
     if (this.finished) {
-      switch(winner) {
-        case -1 -> result.append("\nO WINS!\n");
-        case  1 -> result.append("\nX WINS!\n");
-        default -> result.append("\nDRAW!\n");
+      switch (winner) {
+      case -1 -> result.append("\nO WINS!\n");
+      case  1 -> result.append("\nX WINS!\n");
+      default -> result.append("\nDRAW!\n");
       }
     }
     return result.toString();
